87086a61582d5cd98957afbd10fc971587496ad6
zhoujin <jin100chow@hotamil.com>
2017-07-27 13:50:29 +0800

    add update_temp

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,5 +3,15 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
   end
-  
+
+  def update_temp
+    city = City.find(params[:id])
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                              :params => { :cityname => city.juhe_id, :key => "c39aeac46e841c21257d6ed542551804" }
+    data = JSON.parse(response.body)
+
+    city.update( :current_temp => data["result"]["sk"]["temp"] )
+
+    redirect_to cities_path
+  end
 end
