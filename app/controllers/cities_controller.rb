32ff0e9717d680923f38898b7ea01cdb6b9a602b
zhoujin <jin100chow@hotamil.com>
2017-07-27 14:04:27 +0800

    add protect_api_key

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,7 +7,7 @@ class CitiesController < ApplicationController
   def update_temp
     city = City.find(params[:id])
     response = RestClient.get "http://v.juhe.cn/weather/index",
-                              :params => { :cityname => city.juhe_id, :key => "c39aeac46e841c21257d6ed542551804" }
+                              :params => { :cityname => city.juhe_id, :key => "JUHE_CONFIG["api_key"]" }
     data = JSON.parse(response.body)
 
     city.update( :current_temp => data["result"]["sk"]["temp"] )
